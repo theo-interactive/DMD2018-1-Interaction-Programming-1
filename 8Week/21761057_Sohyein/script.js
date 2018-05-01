60971294ff0edc46c4dd5ad52be2f686ed2f2791
RH <ryonghwon@gmail.com>
2018-05-01 22:55:53 +0900

    소혜인 질문 답변.

--- 8Week/21761057_Sohyein/script.js
+++ 8Week/21761057_Sohyein/script.js
@@ -77,6 +77,7 @@ function percentResult ()
 percentBtn.addEventListener("click", percentResult);
 //혹시 여기는 그냥 퍼센트값 말고 나머지값 구하는게 맞는건가요?
 
+// rh : 네, 나머지 값 구하는게 맞습니다만, 퍼센트 값 구하는 것도 시도해보세요.
 
 /*---------------------------------reset---------------------------------*/
 var resetBtn = document.getElementById("btn-reset");
