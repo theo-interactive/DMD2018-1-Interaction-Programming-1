0dc72880bc2e94c4ba81bbf116418bd6ff51d0e9
RH <ryonghwon@gmail.com>
2018-05-02 11:12:50 +0900

    정경은 질문 답변.

--- 8Week/21761110_Jung Kyungeun/quest2.js
+++ 8Week/21761110_Jung Kyungeun/quest2.js
@@ -75,4 +75,10 @@ function onClickBtnReset(){
 
 //교수님. 저는 var cc= parseInt(DivResult.firstChild.nodeValue); 이렇게 해서, cc 가 typeof를 했을때도 number가 나와서
 //0이 되게 놨습니다. 그래서 Number(DivPlus.innerHTML)이걸, number을 빼고, 그냥 cc 만 하려고 했는데, 안됩니다ㅠ
-//즉, cc + Number(sutjada); 이렇게 연산을 하려고 했는데,, 값이 더해지지 않습니다. 
+//즉, cc + Number(sutjada); 이렇게 연산을 하려고 했는데,, 값이 더해지지 않습니다.
+
+// rh : cc + sutjada 연산했을 때, 연산식은 성립이 되고 있습니다.
+// 첫 로드 후, input radio 중에서 체크된 요소가 없기 때문에 selectNumberNumber() 를 호출해도 sutjada 는 선언이후 값이 할당되어 있지 않은 상태로 됩니다.
+// var sutjada 선언시 초기값으로 0 을 할당하면, selectNumberNumber() 호출시 radio 요소 중 체크된 사항이 없어 연산 결과가 NaN 이 되는 경우는 없을 듯 합니다.
+// 그리고 cc 는 전역에서 선언시 값을 한번만 찾아내고 계속해서 값이 그대로 유지되기 때문에, 클릭 콜백함수에서 DivResult 의 결과값을 cc 값에 업데이트 해야 추가적으로 연산이 이뤄집니다.
+// * onClickBtnPercent() 의 연산 확인바랍니다.
