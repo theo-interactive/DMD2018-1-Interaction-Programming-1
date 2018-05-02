cce30516edd10757fd8f53ff5fde351d76ff7f8d
RH <ryonghwon@gmail.com>
2018-05-02 20:57:03 +0900

    명진웅 질문 답변, 힌트.

--- 8Week/21761034_Myeong Jinwoong/homework/script.js
+++ 8Week/21761034_Myeong Jinwoong/homework/script.js
@@ -78,3 +78,7 @@ _btnreset.addEventListener("click", _resetcalculator);
 
 /* 아는선에서 작성해봤는데.. 어떻게 되긴했는데 더 줄여보고싶은데 잘 안돼요..
 하나의 function안에 click 이벤트에 따라 다른 수식을 할 수 있도록 넣고 싶은데, 더 디벨럽 해보겠습니다. */
+
+// rh : Number(_radio[i].value); 는 radio 의 checked 된 값이 바뀔 때만 변합니다.
+// radio value 에 대한 전역변수를 사용한다면 해당 부분에서 for 문을 계속 해서 찾을 필요는 없을 것 같습니다.
+// 그리고 click 이벤트를 하나만 두고 만든다고 하면, 콜백시 현재의 id 문자열을 찾아서 조건문으로 사용한다면 가능할 것 같은데, 시도해보세요~
