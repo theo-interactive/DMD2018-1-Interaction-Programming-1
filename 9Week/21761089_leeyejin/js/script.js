6029171b786a434f179c532101bc124be59f8b77
USER-PC\USER <lwk0905@naver.com>
2018-05-03 12:07:48 +0900

    제출합니다!

---
+++ 9Week/21761089_leeyejin/js/script.js
@@ -0,0 +1,74 @@
+//02.
+function second(){
+    
+    if(5 === Number) {
+        console.log('true');
+    }else('text'=== String)
+    {
+        console.log('false');
+    }
+
+};
+second();
+
+//03.
+var _object =[
+    greeting ='안녕하세요.',
+    grade = 2,
+    myName = '학년 D반 이예진입니다.',
+    to ='교수님,',
+    feel = '너무 어려워요...',
+    tell = '전 망한거 같습니다. ㅠㅠ분며이 공부를 했는데도 문제보니까 얼어버렸어요.흑ㅜ'
+];
+function Tell (_object){
+    var _type = ' ';
+    for(var i = 0; i < _object.length ; i++){
+        _type += _object[i] ;
+      
+    }
+}
+//console.log(_type);
+
+//04.
+var _array1 = [1,2,5,6,8,9];
+var max =Math.max.apply(null,_array1);
+console.log(max);
+ var _array2= [12,51,66,80,90];
+ var min =Math.min.apply(null,_array2);
+ console.log(min);
+ function four(){
+    
+//     console.log(
+// }
+
+//05.
+var btnevent = document.getElementsByName('btn-student-number');
+btnevent.addEventListener('click', function(){
+    alert('cc');
+});
+
+//06,
+
+//07.
+var btncolor = document.getElementById(btn-color);
+btncolor.addEventListener('click',function(){
+    console.log(click);
+    // if()
+});
+
+//08.
+
+var a =7;
+function _Awww(c){
+    if(c === Number){
+        
+    }else(){
+        alert('숫자만 가능합니다.');
+    }
+}
+
+//09.
+
+//10.
+
+//11.
