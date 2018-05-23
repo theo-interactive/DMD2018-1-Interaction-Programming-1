94fc4b5c790c3f88c0fd7b4a173981f9e97326f4
minho9505 <37163522+minho9505@users.noreply.github.com>
2018-05-23 23:18:31 +0900

    과제제출합니다

---
+++ 11Week/21461018_kimminho/js/script.js
@@ -0,0 +1,40 @@
+var galleryEl = document.getElementById('gallery');
+    var viewEl = galleryEl.getElementsByClassName('view')[0];
+    var viewItemEl = viewEl.getElementsByClassName('view-item');
+    var listEl = galleryEl.getElementsByClassName('list')[0];
+    var listItemEl = listEl.getElementsByTagName('li');
+    var listItemAEl = listEl.getElementsByTagName('a');
+    // var viewElement = galleryElement.children()[0];
+    // var listElement = galleryElement.children()[1];
+
+
+
+    var _cuId = 0;
+    var _exId = _cuId;
+
+    function btnListClick(id){
+      listItemAEl[id].addEventListener('click', function(event){
+        event.preventDefault();
+        console.log('click', id);
+        _cuId = id;
+        changeImage();
+      })
+    }
+    function changeImage(){
+      viewItemEl[_exId].classList.remove('selected');
+      viewItemEl[_cuId].classList.add('selected');
+      _exId = _cuId;
+      // _cuId
+      // viewItemEl[]
+    }
+
+
+    for(var i = 0; i < listItemAEl.length; i ++){
+      btnListClick(i);
+      // listItemAEl[i].addEventListener('click', function(event){
+      //   event.preventDefault();
+      //   console.log('click');
+        
+      // }
+      // )}
+    }
