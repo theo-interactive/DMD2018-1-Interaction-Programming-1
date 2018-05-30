c8b2ed5cc15a3fe96a344b10a13c25f94b413d10
note\다혜 <ydh0606@naver.com>
2018-05-30 17:47:07 +0900

    윤다혜 과제 제출합니다 :)

---
+++ 12Week/21661082_YoonDahye/js/script.js
@@ -0,0 +1,53 @@
+var $tabMenu = document.getElementById ('tab-menu');
+var $tabContainer = document.querySelector ('.tab-container');
+
+var $tabMenuEl = $tabMenu.querySelectorAll ('a');
+var $tabContent = $tabContainer.querySelectorAll ('.tab-content');
+
+
+
+var _current = 0;
+var _exchange = _current;
+
+
+
+function tabMenuClick (id) {
+
+
+	function onClickTabMenu() {
+
+		console.log (id);
+		var $el = this;
+
+		if (!$el.classList.contains('selected')) {
+			_current = id;
+
+			$tabMenuEl[_exchange].classList.remove('selected');
+			$el.classList.add('selected');
+			$tabContent[_exchange].classList.remove('selected');
+			$tabContent[_current].classList.add('selected');
+
+			_exchange = _current;
+		}
+	}
+
+	$tabMenuEl[id].addEventListener('click',onClickTabMenu);
+
+}
+
+
+
+
+function init () {
+
+	console.log ('init');
+
+	for (var i = 0; i < $tabMenuEl.length; i++) {
+		tabMenuClick(i);
+	}
+}
+
+init();
+
+
+
