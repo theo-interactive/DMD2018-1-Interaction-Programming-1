48f2a70424b919533a34647ff3d91ce61dfdbc7b
RH <ryonghwon@gmail.com>
2018-06-07 11:28:13 +0900

    14주차 수업 진행 3.

--- 14Week/#lecture/accordionSlider_new/js/script.js
+++ 14Week/#lecture/accordionSlider_new/js/script.js
@@ -1,32 +1,40 @@
 var $menu = document.getElementById('accordion-menu');
 var $menuItems = $menu.querySelectorAll('.menu-item');
 var $menuItemsEl = $menu.querySelectorAll('a');
+var _max = $menuItems.length;
 var _itemW = 0.4; //40%
 //100 - 1.0
 //40% - 0.4. 20% - 0.2
 function menuOverOut(id){
     function onOverMenu(e){
         e.preventDefault();
-        //console.log('over', id);
-        for(var i = 0; i < $menuItems.length; i++){
+        for(var i = 0; i < _max; i++){
+            //현재 영역에 오버된 요소 - selected.
+            var width = 0;
             if(i === id){
-                $menuItems[i].style.width = 800 * _itemW + 'px';
-                //800px - 40%
+                //현재 선택된 요소 item.
+                //선택된 item 의 width : 800 의 40% = 800 * _itemW(0.4)
+                width = 800 * _itemW; //40%
             }else{
-                $menuItems[i].style.width = 800 * ((1 - _itemW) / ($menuItems.length - 1))  + 'px';
-                //(1 - _itemW) / ($menuItems.length - 1)
-                //over 이외의 요소들의 width.
-                //(1 - _itemW) / ($menuItems.length - 1)
-                //(1.0 - 0.4) / (4 - 1)
-                //0.6 / 3
-                //----800 * 0.2 = 20%
-
+                //현재 선택된 item 이외의 item.
+                //width : 800 의 40% 를 제외한 영역을 기준으로 나머지 개수를 나누어 영역을 구함.
+                //(1) (1.0 - 0.4) - 40% 를 제외한 영역 ---- 0.6
+                //(2) (4 - 1) - 선택된 요소를 제외한 개수 - _max($menuItems.length) - 1  ----- 3
+                //800 * (1) / (2) = 0.2
+                //공식 : 전체영역의 길이  * (1.0 - _itemW) / (_max - 1)
+                width = 800 * (1.0 - _itemW) / (_max - 1); //20%
             }
+            //100% 40-20-20-20
+            $menuItems[i].style.width = width + 'px';            
         }
     }
     function onOutMenu(e){
         e.preventDefault();
-        console.log('out', id);
+        //console.log('out', id);
+        for(var i = 0; i < _max; i++){
+            width = 800 / _max;
+            $menuItems[i].style.width = width + 'px';
+        }
     }
     //마우스 이벤트.
     //click.
@@ -40,6 +48,6 @@ function menuOverOut(id){
     $menuItemsEl[id].addEventListener('mouseenter', onOverMenu);
     $menuItemsEl[id].addEventListener('mouseleave', onOutMenu);
 }
-for(var i = 0; i < $menuItemsEl.length; i++){
+for(var i = 0; i < _max; i++){
     menuOverOut(i);
 }
